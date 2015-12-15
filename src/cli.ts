3bf8c3974fc6ceb2546cbfa501da0386e0f421ab
Tom Shen <tshen@palantir.com>
2015-12-15 11:52:48 -0500

    Support installing/uninstalling more than one package at once

--- src/cli.ts
+++ src/cli.ts
@@ -6,24 +6,27 @@ commander
     .version(require("../package").version);
 
 commander
-    .command("install <name>")
+    .command("install <name> [otherNames...]")
     .description(`install typings to ${TYPINGS_DIR}`)
-    .action((name: string) => {
-        const typing = parseTyping(name);
-        install(typing);
+    .action((name: string, otherNames: string[]) => {
+        [name, ...otherNames]
+            .map(parseTyping)
+            .forEach((typing) => install(typing)); // Don't pass in index, array
     });
 
 commander
-    .command("uninstall <name>")
+    .command("uninstall <name> [otherNames...]")
     .alias("remove")
     .description(`uninstall typings from ${TYPINGS_DIR}`)
-    .action((name: string) => {
-        uninstall(name, (err: any) => {
-            if (err) {
-                console.error(err.message);
-                return;
-            }
-            console.log(`Uninstalled: ${name}`);
+    .action((name: string, otherNames: string[]) => {
+        [name, ...otherNames].forEach((name) => {
+            uninstall(name, (err: any) => {
+                if (err) {
+                    console.error(err.message);
+                    return;
+                }
+                console.log(`Uninstalled: ${name}`);
+            });
         });
     });
 
